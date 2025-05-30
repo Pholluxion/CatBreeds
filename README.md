87943f79cb13e34045b9292b1b0a71a75ad20536
Pholluxion <penalozadaniel00@gmail.com>
2025-05-30 08:07:47 -0500

    feature: splash added

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
 # CatBreed
 
 The CatBreed is a simple web application that allows users to search for cat breeds and view their details. 
+
+## Screenshots
+
+Android and iOS screenshots of the application are available below:
+![Screenshot 1](https://github.com/Pholluxion/CatBreeds/blob/main/assets/images/AndroidScreenshot.png)
+![Screenshot 2](https://github.com/Pholluxion/CatBreeds/blob/main/assets/images/iOSScreenshot.png)
+
+## Features
+- Search for cat breeds by name
+- View details of each breed including origin, temperament, and more
+- Responsive design for mobile
+
+## Technologies Used
+- Flutter
+- Dart
+- Cat API for fetching breed data
+- BloC pattern for state management
+
+## Installation
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/Pholluxion/CatBreeds.git
+    ```
+2. Navigate to the project directory:
+    ```bash
+    cd CatBreeds
+    ```
+3. Install dependencies:
+    ```bash
+    flutter pub get
+    ```
+4. Run the application:
+    ```bash
+    flutter run
+    ```
+## Usage
+- Open the app on your device or emulator.
+- Use the search bar to find a specific cat breed.
+- Click on a breed to view its details.
+
+
+
---
+++ lib/features/breed/presentation/pages/splash_page.dart
@@ -0,0 +1,54 @@
+import 'package:cat_breed/core/core.dart';
+import 'package:cat_breed/features/breed/breed.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+
+const kBackgroundColor = Color(0xFF6A3DE8);
+const kWhiteColor = Color(0xFFFFFFFF);
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<BreedBloc, BreedState>(
+      listener: (context, state) {
+        if (state.status == BreedStatus.success) {
+          context.pushReplacementNamed('home');
+        }
+      },
+      child: Container(
+        color: kBackgroundColor,
+        child: Column(
+          spacing: 20,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'Cat Breed',
+              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 100),
+            Center(
+              child: Image.asset(
+                'assets/images/cat.png',
+                width: 200,
+                height: 200,
+              ),
+            ),
+            UICircularProgressIndicator(),
+            Text(
+              'Loading...',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
