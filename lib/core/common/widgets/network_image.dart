720036b30f05d2695240936689f5d3cadd5de132
Pholluxion <penalozadaniel00@gmail.com>
2025-05-31 13:21:47 -0500

    chore: ordered imports

--- lib/core/common/widgets/network_image.dart
+++ lib/core/common/widgets/network_image.dart
@@ -1,8 +1,9 @@
-import 'package:cat_breed/core/common/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:shimmer/shimmer.dart';
+
+import 'package:cat_breed/core/common/widgets/widgets.dart';
 
 class UINetworkImage extends StatelessWidget {
   final String ref;
